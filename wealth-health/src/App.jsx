bffec603350dfe38a31c60b7554065a7c209d8b6
Olivia L <letendartolivia@gmail.com>
2024-07-30 09:17:59 +0200

    create component navbar + insert the logo in the application

--- wealth-health/src/App.jsx
+++ wealth-health/src/App.jsx
@@ -3,11 +3,13 @@ import DataTable from "./components/DataTable/DataTable.jsx";
 import DateSelection from "./components/DateSelection/DateSelection.jsx";
 import DropdownMenu from "./components/DropdownMenu/DropdownMenu.jsx";
 import ModalWindow from "./components/ModalWindow/ModalWindow.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
 
   return (
     <>
+        <Navbar />
       <div>
           <DataTable />
           <DateSelection />
---
+++ wealth-health/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.scss';
+import logo from '../../assets/logoWealthHealth.png';
+
+export default function Navbar() {
+    return (
+        <>
+            <img src={logo} alt="logo wealth health" />
+        </>
+    )
+}
