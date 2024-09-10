68fea308d7a178a6b2a06783c7088f198e60ba1f
Olivia L <letendartolivia@gmail.com>
2024-09-10 13:56:23 +0200

    stylise the component dateSelection + responsive component dataTable

--- wealth-health/src/components/DateSelection/DateSelection.jsx
+++ wealth-health/src/components/DateSelection/DateSelection.jsx
@@ -3,12 +3,15 @@ import DateTimePicker from 'react-datetime-picker';
 import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
+import {useState} from "react";
 
 export default function DateSelection({htmlFor, text}) {
+    const [value, setValue] = useState(new Date());
+
     return (
         <div className="flexParentDateTime">
             <label htmlFor={htmlFor}>{text}</label>
-            <DateTimePicker />
+            <DateTimePicker onChange={setValue} value={value}/>
         </div>
     )
 }
