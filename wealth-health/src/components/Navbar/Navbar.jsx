94c13dec86b562ec2a52e0b4bea1684537c8fbff
Olivia L <letendartolivia@gmail.com>
2024-09-18 13:55:11 +0200

    import useLocation + check the page and if you are on it, disabled the button

--- wealth-health/src/components/Navbar/Navbar.jsx
+++ wealth-health/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
 import './Navbar.scss';
 import logo from '../../assets/logoWealthHealth.png';
-import {Link} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+    const location = useLocation();
+
     return (
-        <navbar>
+        <nav>
             <ul>
                 <li>
                     <Link to="/">
-                        <img src={logo}
-                            alt="logo wealth health"/>
+                        <img src={logo} alt="logo wealth health"/>
                     </Link>
                 </li>
                 <li>
-                    <Link to="current-employess">
-                        <button>View current employees</button>
-                    </Link>
+                    {location.pathname === "/current-employess" ? (
+                        <button disabled>View current employees</button>
+                    ) : (
+                        <Link to="/current-employess">
+                            <button>View current employees</button>
+                        </Link>
+                    )}
                 </li>
             </ul>
-        </navbar>
-    )
+        </nav>
+    );
 }
