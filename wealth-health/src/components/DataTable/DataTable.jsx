f87240a328e06db907f482468ce5646375601af2
Olivia L <letendartolivia@gmail.com>
2024-10-15 12:54:42 +0200

    correction so that the length is equal to or greater than 1 + creation of a “cell” in the table's columns const to display a tooltip when hovering over it and see the complete data

--- wealth-health/src/components/DataTable/DataTable.jsx
+++ wealth-health/src/components/DataTable/DataTable.jsx
@@ -9,44 +9,55 @@ export default function DataTableComponent() {
         {
             name: 'First Name',
             selector: row => row.firstName,
+            cell: row => <div title={row.firstName}>{row.firstName}</div>
         },
         {
             name: 'Last Name',
             selector: row => row.lastName,
+            cell: row => <div title={row.lastName}>{row.lastName}</div>
         },
         {
             name: 'Street',
             selector: row => row.street,
+            cell: row => <div title={row.street}>{row.street}</div>
         },
         {
             name: 'City',
             selector: row => row.city,
+            cell: row => <div title={row.city}>{row.city}</div>
         },
         {
             name: 'Zip code',
             selector: row => row.zipCode,
+            cell: row => <div title={row.zipCode}>{row.zipCode}</div>
         },
         {
             name: 'State',
             selector: row => row.state,
+            cell: row => <div title={row.state}>{row.state}</div>
         },
         {
             name: 'Department',
             selector: row => row.department,
+            cell: row => <div title={row.department}>{row.department}</div>
         },
         {
             name: 'Birthdate',
             selector: row => row.birthDate,
+            cell: row => <div title={row.birthDate}>{row.firstName}</div>
         },
         {
             name: 'Start Date',
             selector: row => row.startDate[0] + ' to ' + row.startDate[1],
+            cell: row => <div title={row.startDate[0] + ' to ' + row.startDate[1]}>
+                {row.startDate[0] + ' to ' + row.startDate[1]}
+            </div>,
         },
     ];
 
     return (
         <>
-            {employeeData.length > 1 ? (
+            {employeeData.length >= 1 ? (
                 <DataTable
                     columns={columns}
                     data={employeeData}
