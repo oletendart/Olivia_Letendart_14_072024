bd69ab389c88f6745dc7c244f1a2110d62b48fb9
Olivia L <letendartolivia@gmail.com>
2024-12-30 18:50:57 +0100

    insert in the colonne "sortable" and boolean value true in the file DataTable.jsx

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -10,7 +10,6 @@ export default function DataTableComponent() {
     const [filteredData, setFilteredData] = useState(employeeData); 
     const [currentPageData, setCurrentPageData] = useState([]);
 
-    
     const handlePageChange = useCallback((data) => {
         setCurrentPageData(data);
     }, []); 
@@ -23,46 +22,55 @@ export default function DataTableComponent() {
         {
             name: 'First Name',
             selector: row => row.firstName,
+            sortable: true, 
             cell: row => <div title={row.firstName}>{row.firstName}</div>
         },
         {
             name: 'Last Name',
             selector: row => row.lastName,
+            sortable: true,
             cell: row => <div title={row.lastName}>{row.lastName}</div>
         },
         {
             name: 'Street',
             selector: row => row.street,
+            sortable: true,
             cell: row => <div title={row.street}>{row.street}</div>
         },
         {
             name: 'City',
             selector: row => row.city,
+            sortable: true,
             cell: row => <div title={row.city}>{row.city}</div>
         },
         {
             name: 'Zip code',
             selector: row => row.zipCode,
+            sortable: true,
             cell: row => <div title={row.zipCode}>{row.zipCode}</div>
         },
         {
             name: 'State',
             selector: row => row.state,
+            sortable: true,
             cell: row => <div title={row.state}>{row.state}</div>
         },
         {
             name: 'Department',
             selector: row => row.department,
+            sortable: true,
             cell: row => <div title={row.department}>{row.department}</div>
         },
         {
             name: 'Birthdate',
             selector: row => row.birthDate,
+            sortable: true,
             cell: row => <div title={row.birthDate}>{row.birthDate}</div>
         },
         {
             name: 'Start Date',
             selector: row => row.startDate[0] + ' to ' + row.startDate[1],
+            sortable: true,
             cell: row => <div title={row.startDate[0] + ' to ' + row.startDate[1]}>
                 {row.startDate[0] + ' to ' + row.startDate[1]}
             </div>,
@@ -73,26 +81,23 @@ export default function DataTableComponent() {
         <>
         {employeeData.length >= 1 ? (
             <>
-                {/* Barre de recherche */}
                 <Search data={employeeData} onSearchResults={handleSearchResults} />
                 
-                {/* Affichage conditionnel */}
                 {filteredData.length === 0 ? (
                     <p className="error-message">No data found for the search term.</p>
                 ) : (
                     <>
-                        {/* Tableau des données */}
                         <DataTable
                             columns={columns}
                             data={currentPageData}
                             className="dataTable"
+                            defaultSortFieldId={1} 
                         />
                         
-                        {/* Pagination */}
                         <Pagination
                             itemsPerPage={10}
-                            data={filteredData} // Utilisation des données filtrées
-                            onPageChange={handlePageChange} // Pass the memoized function here
+                            data={filteredData} 
+                            onPageChange={handlePageChange}
                         />
                     </>
                 )}
